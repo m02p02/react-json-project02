dd0e3a4b24e863b3c7cb2aacf24d8a79991fc84a
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-16 16:42:52 +0100

    master commit test

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
  <h1>hello world</h1>
-  )
+ <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem itaque doloribus, impedit quis quos porro nostrum! Sunt, molestiae? Eaque sequi debitis dolor consequatur doloremque quaerat? Nam optio vel voluptatibus natus?</p>
+ </>)
 }
 
 export default App
