0cc3ffd28ec4ac5f74789f5c4d81732b137bc9d4
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-16 17:30:01 +0100

    test update bubblemaster

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,7 @@ function App() {
   return (
 
     <>
-      <h1>hello world</h1>
-      <p>our project is cool!😂😂😂😂</p>
-      <h3>what the heck</h3>
-      <p>bararararara</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem itaque doloribus, impedit quis quos porro nostrum! Sunt, molestiae? Eaque sequi debitis dolor consequatur doloremque quaerat? Nam optio vel voluptatibus natus?</p>
+      <h1>update bubblemaster</h1>
     </>
   )
 }
