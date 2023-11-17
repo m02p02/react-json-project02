83b472b7290427fa977fb83dc17eb165ece9e50a
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-17 15:49:27 +0100

    header navbar shenanigans

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
 import { Route, Routes} from "react-router-dom"
+
+import Header from './components/Header.jsx';
 import NavBar from "./components/NavBar"
 import HomePage from "./pages/HomePage"
 import AddLocationPage from "./pages/AddLocationPage"
 import LocationDetailsPage from "./pages/LocationDetailsPage"
+import Footer from './components/Footer.jsx';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-
+    
     <>
       <h1>The mighty locations</h1>
+
+      <Header />      
       <NavBar />
+
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/location-details' element={<LocationDetailsPage/>}/>
         <Route path='/add-location' element={<AddLocationPage/>}/>
       </Routes>
+
+      <Footer />
+
     </>
   )
 }
