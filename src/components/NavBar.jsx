1a13493ccbd87cfdfc28eff43bb14159a5cd842a
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-17 15:34:23 +0100

    something something NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+    return (
+        <div className='navbar'>
+            <Link to='/'>Home</Link>
+        </div>
+    )
+}
+
+export default NavBar;
