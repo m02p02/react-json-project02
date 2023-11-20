8cabca4639070f9fd9c25b6dd98b45637d2e7c8f
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-20 15:41:50 +0100

    combined DeleteLocation.jsx and EditLocation.jsx into EditDeleteLocation.jsx in the components folder

---
+++ src/components/EditDeleteLocation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+
+function EditDeleteLocation() {
+
+    const [place, setPlace] = useState({});
+
+    return (
+        <>
+            <h1>
+                This is Edit-Delete
+            </h1>
+        </>
+    )
+}
+
+export default EditDeleteLocation;
