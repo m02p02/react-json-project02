e156d4d6b7c7c9856a574f2974d8f410df5b6946
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-20 15:01:31 +0100

    starting DeleteLocation

---
+++ src/components/DeleteLocation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from "react";
+
+function DeleteLocation() {
+
+    const [place, setPlace] = useState({});
+
+    return ()
+}
+
+export default DeleteLocation;
