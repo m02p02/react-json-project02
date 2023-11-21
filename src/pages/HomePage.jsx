9f034c7406398189da2136d232d4a54f1b77c0fe
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-21 15:36:46 +0100

    homepage buttons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,22 +7,22 @@ function HomePage() {
 
     return (
         <>
-        <div>
-    
-            {/* <h1>Galaxy Map with links</h1> */}
-            <nav className="galaxyMap">
-                <span>
-                <Button variant="outline-secondary"><Link to="/places/random-place">The random place</Link></Button>{"  "}
-                </span>
-                <span>
-                <Button variant="outline-secondary"><Link to="/places/add-place">Add your new discovery</Link></Button>{"  "}
-                </span>
-                <span>
-                <Button variant="outline-secondary"><Link to='/places/all-places'>Check what`s discovered</Link></Button>{"  "}
-                </span>
-            </nav>
+            <div>
 
-        </div>
+                {/* <h1>Galaxy Map with links</h1> */}
+                <div className="galaxy-map">
+                    <span className='hp-random-btn'>
+                        <Button variant="outline-secondary"><Link to="/places/random-place">The random place</Link></Button>{"  "}
+                    </span>
+                    <span>
+                        <Button variant="outline-secondary"><Link to="/places/add-place">Add your new discovery</Link></Button>{"  "}
+                    </span>
+                    <span>
+                        <Button variant="outline-secondary"><Link to='/places/all-places'>Check what`s discovered</Link></Button>{"  "}
+                    </span>
+                </div>
+
+            </div>
         </>
     )
 }
