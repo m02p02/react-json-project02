b44e92200b922c30acbe0b22dca31c2581a74afe
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-21 16:14:48 +0100

    homepage buttons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { Button } from "react-bootstrap";
 
 
 function HomePage() {
-    
+
 
     return (
         <>
@@ -13,16 +13,16 @@ function HomePage() {
                 {/* <h1>Galaxy Map with links</h1> */}
                 <div className="galaxy-map">
                     <span className='hp-random-btn'>
-                        <Button variant="outline-secondary"><Link to="/places/random-place">The random place</Link></Button>{"  "}
+                        <Button variant="outline-secondary"><Link to="/places/random-place">Random Destination</Link></Button>{"  "}
                     </span>
                     <span className='hp-random-btn2'>
-                        <Button variant="outline-secondary"><Link to="/places/add-place">Add your new discovery</Link></Button>{"  "}
+                        <Button variant="outline-secondary"><Link to="/places/add-place">Log Destination</Link></Button>{"  "}
                     </span>
                     <span className='hp-random-btn3'>
-                        <Button variant="outline-secondary"><Link to='/places/all-places'>Check what`s discovered</Link></Button>{"  "}
+                        <Button variant="outline-secondary"><Link to='/places/all-places'>Load Database</Link></Button>{"  "}
                     </span>
                     <span className='hp-random-btnEarth'>
-                        <Button variant="outline-secondary"><Link to='//places/details/1'>The Earth</Link></Button>{"  "}
+                        <Button variant="outline-secondary"><Link to='/places/details/1'>Earth</Link></Button>{"  "}
                     </span>
                 </div>
 
