eee8426b2c087560becc3904bf10557496f38c50
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-22 19:34:12 +0100

    cleaning searchBar from Master

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import SearchBar from '/SearchBar';
 
 function Header() {
 
-    const handleSearch = (results) => {
-
-    }
-
     return (
-        <div className='header-footer-bg'><SearchBar /></div>
+        <div className='header-footer-bg'></div>
     )
 }
 
