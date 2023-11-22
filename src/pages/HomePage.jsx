500b20377eb4b455a2539b93714de8d7d4233941
yvzbly <yavuzbalay90@gmail.com>
2023-11-22 11:35:25 +0100

    home page modal window done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,53 +1,71 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { Button, Modal } from "react-bootstrap";
 
 function HomePage() {
   const [showModal, setShowModal] = useState(false);
+  const [urlToPreview, setUrlToPreview] = useState("");
 
   const handleShowModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
+
+  const handlePreviewUrl = (url) => {
+    setUrlToPreview(url);
+    handleShowModal();
+  };
 
   return (
     <>
       <div>
         <div className="galaxy-map">
-          <span className='hp-random-btn'>
+          <span className="hp-random-btn">
             <Button variant="light">
-              <Link to="/places/random-place">Random Destination</Link>
+              <a href="/places/random-place">Random Destination</a>
             </Button>{" "}
           </span>
-          <span className='hp-random-btn2'>
+          <span className="hp-random-btn2">
             <Button variant="light">
-              <Link to="/places/add-place">Log Destination</Link>
+              <a href="/places/add-place">Log Destination</a>
             </Button>{" "}
           </span>
-          <span className='hp-random-btn3'>
+          <span className="hp-random-btn3">
             <Button variant="light">
-              <Link to='/places/all-places'>Load Database</Link>
+              <a href="/places/all-places">Load Database</a>
             </Button>{" "}
           </span>
 
-          <span className='hp-random-btnEarth'>
-            <Button variant="light" onClick={handleShowModal}>
+          <span className="hp-random-btnEarth">
+            <Button
+              variant="light"
+              onClick={() =>
+                handlePreviewUrl(
+                  "https://project-exo-app.netlify.app/places/details/1"
+                )
+              }
+            >
               Earth
             </Button>{" "}
           </span>
         </div>
 
-        <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal show={showModal} onHide={handleCloseModal} size="lg">
           <Modal.Header closeButton>
             <Modal.Title>Earth Details</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            
+            {urlToPreview && (
+              <iframe
+                title="URL Preview"
+                src={urlToPreview}
+                style={{ width: "100%", height: "500px", border: "none" }}
+              ></iframe>
+            )}
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleCloseModal}>
               Close
             </Button>
           </Modal.Footer>
-    </Modal>
+        </Modal>
       </div>
     </>
   );
